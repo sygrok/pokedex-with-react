a558f52702b3938125bda1c0a42e80fda8db8f49
Berkay Anduv <sygrok@hotmail.com>
2023-11-23 07:44:03 +0300

    HeaderLink

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,18 @@
 import "./Header.css";
 import downArrow from "../assets/down-arrow.png";
 import pokeball from "../assets/pokeball.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Pokedex App</h1>
-      <img className="pokeball" src={pokeball} />
+    <Link style={{ textDecoration: "none", color: "white" }} to={"/"}>
+      <header>
+        <h1>Pokedex App</h1>
 
-      {/* <img className="down-arrow" src={downArrow} /> */}
-      <h3>By Berkay Anduv</h3>
-    </header>
+        <img className="pokeball" src={pokeball} />
+
+        <h3>By Berkay Anduv</h3>
+      </header>
+    </Link>
   );
 }
