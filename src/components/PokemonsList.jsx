e245fc9fbeea7941fcc76b67ea5ac996f5c05c69
Berkay Anduv <sygrok@hotmail.com>
2023-11-23 07:52:36 +0300

    FilterByCategory

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -3,7 +3,9 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
+  //fetch data
   const fetchData = useCallback(async () => {
     const response = await fetch(
       "https://pokedex-app-with-react-default-rtdb.firebaseio.com/pokemons.json"
@@ -28,9 +30,26 @@ export default function PokemonList() {
     fetchData();
   }, [fetchData]);
 
+  //Filter by category
+  const filteredByCategory = (x) => {
+    setSelectedCategory(x);
+  };
+  const getFilteredPokemons = () => {
+    if (selectedCategory === null) {
+      return pokemons;
+    } else {
+      return pokemons.filter((x) => x.category === selectedCategory);
+    }
+  };
+
   return (
     <>
-      <PokemonListItem event={pokemons} />
+      <section>
+        <button onClick={() => filteredByCategory(null)}>All</button>
+        <button onClick={() => filteredByCategory("fire")}>Fire</button>
+        <button onClick={() => filteredByCategory("electric")}>Electric</button>
+      </section>
+      <PokemonListItem event={getFilteredPokemons()} />
     </>
   );
 }
