49cbeccb8e580c25cba630cc0544947ebf91d0b9
Berkay Anduv <sygrok@hotmail.com>
2023-11-26 14:52:52 +0300

    PokemonPage&EditPokemonPageStyling

--- src/pages/EditPokemonPage.jsx
+++ src/pages/EditPokemonPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import "./EditPokemonPage.css";
 
 export default function EdipPokemon() {
   const navigate = useNavigate();
@@ -52,44 +53,66 @@ export default function EdipPokemon() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={editedPokemon.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Image URL:
-        <input
-          type="text"
-          name="img"
-          value={editedPokemon.img}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Category:
-        <input
-          type="text"
-          name="category"
-          value={editedPokemon.category}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Description:
-        <textarea
-          name="desc"
-          value={editedPokemon.desc}
-          onChange={handleChange}
-        />
-      </label>
+    <>
+      <section className="edit-section-order">
+        <div className="pokebox-edit">
+          <div>
+            <h1>{editedPokemon.name}</h1>
+            <img src={editedPokemon.img} alt={editedPokemon.name} />
+            <h2>{editedPokemon.category}</h2>
+            <p>{editedPokemon.desc.substring()}</p>
+          </div>
+        </div>
+        <div className="editbox">
+          <form onSubmit={handleSubmit}>
+            <label>Name</label>
 
-      <button type="submit">Save Changes</button>
-    </form>
+            <input
+              type="text"
+              name="name"
+              value={editedPokemon.name}
+              onChange={handleChange}
+            />
+            <label>Image</label>
+            <input
+              type="text"
+              name="img"
+              value={editedPokemon.img}
+              onChange={handleChange}
+            />
+
+            <label>Category</label>
+
+            <select
+              name="category"
+              value={editedPokemon.category}
+              onChange={handleChange}
+            >
+              <option>fire</option>
+              <option>electric</option>
+              <option>water</option>
+              <option>grass</option>
+              <option>flying</option>
+              <option>fairy</option>
+              <option>poison</option>
+              <option>fighting</option>
+              <option>psychic</option>
+              <option>rock</option>
+              <option>ghost</option>
+            </select>
+
+            <label>Description</label>
+
+            <textarea
+              name="desc"
+              value={editedPokemon.desc}
+              onChange={handleChange}
+            />
+
+            <button type="submit">Save Changes</button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -1,6 +1,9 @@
 import "./PokemonPage.css";
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import homeArrow from "../assets/home-arrow.png";
+import deleteIcon from "../assets/delete-icon.png";
+import editIcon from "../assets/edit-icon.png";
 
 export default function PokemonPage() {
   const { id } = useParams();
@@ -49,14 +52,30 @@ export default function PokemonPage() {
 
   return (
     <>
-      <section className="pokebox">
-        <h1>{selectedPokemon.name}</h1>
-        <img src={selectedPokemon.img} alt={selectedPokemon.name} />
-        <h2>{selectedPokemon.category}</h2>
-        <p>{selectedPokemon.desc.substring()}</p>
-
-        <button onClick={handleDelete}>delete</button>
-        <button onClick={navigateEdit}>edit</button>
+      <section className="orderbox">
+        <div className="back">
+          <Link style={{ textDecoration: "none", color: "#e3e3e3" }} to="/">
+            <img src={homeArrow} />
+            <p>Go Home</p>
+          </Link>
+        </div>
+        <div className="pokebox">
+          <div>
+            <h1>{selectedPokemon.name}</h1>
+            <img src={selectedPokemon.img} alt={selectedPokemon.name} />
+            <h2>{selectedPokemon.category}</h2>
+            <p>{selectedPokemon.desc.substring()}</p>
+            <div className="pokebox-buttons">
+              <div onClick={handleDelete}>
+                <img src={deleteIcon} />
+              </div>
+              <div onClick={navigateEdit}>
+                <img className="edit" src={editIcon} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div></div>
       </section>
     </>
   );
